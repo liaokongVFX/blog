3b3e943e7b0a331832f2281ba4ed4c7a206e4ab8
admin <admin@example.com>
2019-04-24 22:10:01 +0800

    push toy

---
+++ blueprints/__init__.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+# Time    : 2019/4/24 22:08
+# Author  : LiaoKong
+
+
+
---
+++ blueprints/auth.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+# Time    : 2019/4/24 22:08
+# Author  : LiaoKong
+
+from flask import Blueprint
+
+auth_bp = Blueprint("auth", __name__)
